9bd13a27a60b5af511f050f4cbc931772b1fb50b
Thomas <thomaspegg.linck@gmail.com>
2021-04-01 23:02:34 -0300

    start unit testing

--- speed_test.py
+++ speed_test.py
@@ -2,18 +2,25 @@ import speedtest
 from datetime import datetime
 import pandas as pd
 from threading import Timer
+import unittest
+
+
+class internet_function_test(unittest.TestCase):
+    def test_internet_function(self):
+        self.assertEqual([retrieve_current.date(), retrieve_current.time(), round(retrieve_current.internet_speed())],
+                         internet(lambda parameters_list: parameters_list))
 
 
 class retrieve_current:
-    @staticmethod
+    @ staticmethod
     def time():
         return datetime.now().strftime('%H:%M')
 
-    @staticmethod
+    @ staticmethod
     def date():
         return datetime.now().strftime('%d/%m/%Y')
 
-    @staticmethod
+    @ staticmethod
     def internet_speed():
         return speedtest.Speedtest().download(threads=None)*(10**-6)
 
@@ -24,13 +31,16 @@ def write_to_excel(parameters_list):
     df.to_excel('dados.xlsx', sheet_name='base', index=False)
 
 
-def internet():
-    print("starting")
-    write_to_excel([retrieve_current.date(),
+def internet(write_function):
+    write_function([retrieve_current.date(),
                     retrieve_current.time(),
                     round(retrieve_current.internet_speed())])
 
 
 def execute_application():
-    internet()
+    internet(write_to_excel)
     Timer(1, execute_application).start()
+
+
+unittest.main()
+execute_application()
