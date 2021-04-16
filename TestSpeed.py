a9c2f34a96179cd7247098d1e0f81ec7c3c71986
WinnterWolf <patrick.winter@outlook.com.br>
2021-04-16 15:11:02 -0300

    alteracoes de logs e melhoria no codigo

--- TestSpeed.py
+++ TestSpeed.py
@@ -3,6 +3,8 @@ from datetime import datetime
 import pandas as pd
 from threading import Timer
 
+minutos = 20
+
 # função para gravar dados da velocidade da internet
 def internet():
     df = pd.read_excel('dados.xlsx', sheet_name='base')
@@ -10,11 +12,18 @@ def internet():
     data_atual = datetime.now().strftime('%d/%m/%Y')
     hora_atual = datetime.now().strftime('%H:%M')
     velocidade = s.download(threads=None)*(10**-6)
+    print(data_atual)
+    print(hora_atual)
+    print(round(velocidade))
+
     df.loc[len(df)] = [data_atual, hora_atual, round(velocidade)]
     df.to_excel('dados.xlsx', sheet_name='base', index=False)
-    Timer(30,internet).start()
+    print("Registro feito na planilha. " + hora_atual + " | Velocidade download: " + str(round(velocidade)))
 
+    
+    Timer((60*minutos), internet).start()
 
+print("Start timer, executar a cada " + str((60*minutos)/60) + " minutos")
 internet()
 
 
